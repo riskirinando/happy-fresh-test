13674a6b6755b50ffe77c0edaee3f17e072ad972
riskirinando <79828572+riskirinando@users.noreply.github.com>
2022-03-15 19:57:22 +0700

    Delete src directory

--- src/api/middlewares/default.js
+++
@@ -1,6 +0,0 @@
-// Return 404 error if there's no matching route
-const defaultRouteHandler = (req, res) => {
-    res.status(404).send('Route does not exist')
-}
-
-module.exports = defaultRouteHandler
